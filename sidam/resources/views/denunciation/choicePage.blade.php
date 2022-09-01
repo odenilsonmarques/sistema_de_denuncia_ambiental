2a07bc0df41b612d0a2bc11afb40d1087db2c717
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-09-01 10:02:06 -0300

    criacao do modal

--- sidam/resources/views/denunciation/choicePage.blade.php
+++ sidam/resources/views/denunciation/choicePage.blade.php
@@ -14,10 +14,28 @@
                 <h6>ainda não possui uma conta, cadastre-se</h6>
             </div>
             <div class="col-sm-4 text-center mt-5">
-                <a href="{{route('denunciatoin.anonymous')}}">
+                <a href="{{route('denunciatoin.anonymous')}}" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <h2>Denuncia Anônima</h2>
                     <h6>você pode optar pela denúncia Anônima</h6>
                 </a>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Atenção</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Se você optar pela denúncia Anônima, não poderá acompanhar o andamento da denúncia por esse canal.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button type="button" class="btn btn-primary"><a href="{{route('denunciatoin.anonymous')}}">Continuar</a> </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
