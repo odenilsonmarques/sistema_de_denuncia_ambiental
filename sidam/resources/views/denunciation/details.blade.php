ff8d38de03e849491a74194cdca94876850a6906
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-09-05 21:54:53 -0300

    ajuste na pagina de detalhes

--- sidam/resources/views/denunciation/details.blade.php
+++ sidam/resources/views/denunciation/details.blade.php
@@ -9,36 +9,45 @@
         <div class="row mt-5">
             <div class="col-sm-3">
                 <ul class="">
-                    <li class="list-group-item active" aria-current="true">Categoria</li>
+                    <li class="list-group-item list-group-item-dark" aria-current="true">Categoria</li>
                     <li  class="list-group-item">{{$data->category}}</li>
+                    <li class="list-group-item list-group-item-dark" aria-current="true">Local da ocorrência</li>
+                    <li  class="list-group-item"><strong>Bairro:</strong> {{$data->distric}}</li>
+                    <li  class="list-group-item"><strong>Rua:</strong> {{$data->road}}</li>
+                    <li  class="list-group-item"><strong>Nº:</strong> {{$data->number}}</li>
+                    <li  class="list-group-item"><strong>Referência:</strong> {{$data->reference_point}}</li>
                 </ul>
             </div>
-            <div class="col-sm-3">
+            
+            <div class="col-sm-4">
                 <ul>
-                    <li class="list-group-item active" aria-current="true">Local da ocorrência</li>
-                    <li  class="list-group-item">Bairro: {{$data->distric}}</li>
-                    <li  class="list-group-item">Rua: {{$data->road}}</li>
-                    <li  class="list-group-item">Nº: {{$data->number}}</li>
-                    <li  class="list-group-item">Referência: {{$data->reference_point}}</li>
-                </ul>
-            </div>
-            <div class="col-sm-6">
-                <ul>
-                    <li class="list-group-item active" aria-current="true">Dados da denuncia</li>
+                    <li class="list-group-item list-group-item-dark" aria-current="true">Dados da denuncia</li>
                     @if($data->violator)
-                        <li class="list-group-item">Infrator: {{$data->violator}}</li>
+                        <li class="list-group-item"><strong>Provável Infrator: </strong>{{$data->violator}}</li>
                     @else
                         <li  class="list-group-item">Infrator: Não identificado</li>
                     @endif
-                    <p>Anexos<p>
-                    <img src="{{asset('storage/'.$data->annex_one)}}" alt="anexo um" width="120" height="90" >
-                    <img src="{{asset('storage/'.$data->annex_two)}}" alt="anexo dois" width="120" height="90" >
-                    <img src="{{asset('storage/'.$data->annex_three)}}" alt="anexo tres" width="120" height="90" >
+                    <li class="list-group-item list-group-item-dark" aria-current="true">Descrição</li>
+                    <li class="list-group-item">{{$data->description}}</li>
                 </ul>
             </div>
-            {{-- <div class="col-sm-3 text-center">
-                <h5>Evidências</h5>
-            </div> --}}
+
+            <div class="col-sm-5">
+                <ul>
+                    <li class="list-group-item list-group-item-dark" aria-current="true">Evidências</li>
+                    <div class="mt-2">
+                        <img src="{{asset('storage/'.$data->annex_one)}}" class="img-thumbnail" alt="anexo um" width="136" height="100" >
+                        @if($data->annex_two and $data->annex_three)
+                            <img src="{{asset('storage/'.$data->annex_two)}}" class="img-thumbnail" alt="anexo dois" width="136" height="100" >
+                            <img src="{{asset('storage/'.$data->annex_three)}}" class="img-thumbnail" alt="anexo tres" width="136" height="100" >
+                        @else
+                            <img class="img-thumbnail"  width="136" height="100" >
+                            <img  class="img-thumbnail"  width="136" height="100" >
+                        @endif
+
+                    </div>
+                </ul>
+            </div>
         </div>
     </div>
 @endsection
