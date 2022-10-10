96d9249539397e9d1ebccc73f0620f5b78e124ba
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-10-10 15:07:16 -0300

    altereacao no layout da pagina home

--- sidam/resources/views/welcome.blade.php
+++ sidam/resources/views/welcome.blade.php
@@ -4,6 +4,15 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-lg-12 inforHome">
+                <img src="{{asset('assets/img/brazao1.png')}}">
+                <h1>PREFEITURA MUNICIPAL DE SÃO JOSÉ DE RIBAMAR</h1>
+                <h2>SECRETARIA MUNICIPAL DO AMBIENTE - SEMAM</h2><hr>
+            </div>
+        </div>
+
+
+        <div class="row">
             <div class="col-sm-6 text-center mt-5" style="border:1px solid red">
                 <a href="{{route('typeDenunciation.choice')}}">Denunciar</a>
                 
