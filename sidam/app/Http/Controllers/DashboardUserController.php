f7bf61d05d6cb87e15507b61ec0b3383b7e0968b
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-10-12 17:29:45 -0300

    exibindo o total de denuncias por usuario

--- sidam/app/Http/Controllers/DashboardUserController.php
+++ sidam/app/Http/Controllers/DashboardUserController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Denunciation_with_identification;
 
 class DashboardUserController extends Controller
 {
     //
     public function dash()
     {
-        return view('dashboard.dashboardUser');
+        $allsDenunciationUser = Auth::user()->denunciation_with_identifications()->count();
+
+        return view('dashboard.dashboardUser',[
+            'allsDenunciationUser' => $allsDenunciationUser,
+        ]);
     }
 }
--- sidam/resources/views/dashboard/dashboardUser.blade.php
+++ sidam/resources/views/dashboard/dashboardUser.blade.php
@@ -8,9 +8,14 @@
                 <h1>Bem - vindo !</h1>
                 <h3>Olá, {{ Auth::user()->name }}</h3>
             </div>
-
             <a href="{{route('denunciation.identification.add')}}">realizar denuncia</a>
+            <div class="col-lg-3 mt-5">
+                    <div class="card-body text-center">
+                      <p class="card-title ">Total de Denuncias</p>
+                      <h5>{{$allsDenunciationUser}}</h5>
+                    </div>
 
+            </div>
         </div>
     </div>
 @endsection
