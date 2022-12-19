10aa9b16c7805902ef31e8224e8e3f562e793ddf
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-12-19 09:01:45 -0300

    insercao do card para o usuario criar conta

--- sidam/resources/views/typeComplaint/choicePage.blade.php
+++ sidam/resources/views/typeComplaint/choicePage.blade.php
@@ -8,11 +8,20 @@
             <div class="col-sm-12 text-center mt-5">
                 <button type="button" class="btn btn-success btn-lg">
                     <a href="{{route('login')}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                         </svg><hr>
                         <strong>Acessando sua conta</strong>
+                    </a>
+                </button>
+                <button type="button" class="btn btn-primary btn-lg">
+                    <a href="{{route('register')}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                          </svg><hr>
+                        <strong>Criando uma conta</strong>
                     </a>
                 </button>          
                 <button type="button" class="btn bg-secondary btn-lg">
